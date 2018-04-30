d6938d7d1383565a4acd584cd9ee3b2e2ac0166f
Leigh Salvador <lssalvador@csu.fullerton.edu>
2018-04-30 13:14:34 -0700

    regex test

--- app/controllers/play.js
+++ app/controllers/play.js
@@ -1,21 +1,28 @@
 import Controller from '@ember/controller';
 import {
+  and,
   gte,
+  match,
   not
 } from '@ember/object/computed';
 
 export default Controller.extend({
 
   guess: '',
-
+  incorrectGuess: false,
+// /[^\s]/g
+  isNoSpace: match('guess', /^\w+/g),
   isValidGuessLen: gte('guess.length', 1),
-  isDisabled: not('isValidGuessLen'),
+  isValid: and('isNoSpace', 'isValidGuessLen'),
+  isDisabled: not('isValid'),
 
   actions: {
     //checks whether guess is correct/incorrect
     sendGuess() {
       alert(`Your Guess: ${this.get('guess')}`);
-      this.set('guessIncorrect', 'moop');
+
+      this.set('incorrectGuess', true);
+      this.$('guess').append('guess');
       this.set('guess', '');
     }
   }
