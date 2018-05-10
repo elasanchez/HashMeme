ee0647d8408e933eba3cbb7e70aad388fa2a5ec2
elasanchez <luigi.easanchez@gmail.com>
2018-05-10 02:08:17 -0700

    removed prompt from this file

--- app/controllers/tutorial.js
+++ app/controllers/tutorial.js
@@ -2,19 +2,4 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
 
-  actions: {
-    loadbox: function() {
-      var newUser = prompt("Please enter your name:");
-      if (newUser == null || newUser == "") {
-        newUser = prompt("Please enter your name:");
-      } else {
-        var newUsername = this.store.createRecord('user', {
-          username: newUser
-        });
-        newUsername.save();
-      }
-
-    }
-  }
-
 });
