d3f76484fda7319d8cb0caa9ff2fc3763a015380
elasanchez <luigi.easanchez@gmail.com>
2018-05-10 02:07:46 -0700

    removed prompt from these pages

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -2,19 +2,4 @@ import Component from '@ember/component';
 
 export default Component.extend({
 
-  actions: {
-    loadbox1() {
-      var newUser = prompt("Please enter your name:");
-      if (newUser == null || newUser == "") {
-        newUser = prompt("Please enter your name:");
-      } else {
-        var newUsername = this.store.createRecord('user', {
-          username: newUser
-        });
-        newUsername.save();
-      }
-
-    }
-  }
-
 });
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,40 +1,5 @@
 import Controller from '@ember/controller';
 
-import {
-  UserStore
-} from './storage';
-
-let userStore = new UserStore('x-hashmeme/u');
-
 export default Controller.extend({
 
-  actions: {
-    loadbox: function() {
-      //Yash code
-      // var newUser = prompt("Please enter your name:");
-      // if (newUser == null || newUser == "") {
-      //   newUser = prompt("Please enter your name:");
-      // } else {
-      //   var newUsername = this.store.createRecord('user', {
-      //     username: newUser
-      //   });
-      //   newUsername.save();
-      // }
-      //END Yash code
-
-      let username = userStore.get();
-      if (!username) { // covers null
-          username = prompt("Please enter your name:");
-
-          //saves to the local session storage
-          userStore.set(username);
-
-          //SAVE TO DATABASE - uncomment luigi
-          // var newUsername = this.store.createRecord('user', {
-          //   username: username
-          // });
-          // newUsername.save();
-      }
-    }
-  }
 });
